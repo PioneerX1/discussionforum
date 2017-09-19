f3744fb0a1f9f3f8493733f33d7bd16dfb6756d3
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-19 15:53:32 -0700

    confirm category detail is invoked when a category is clicked on, also add initial xml to category detail activity

--- app/src/main/java/com/pioneerx1/discussionforum/ui/CategoryDetailActivity.java
+++ app/src/main/java/com/pioneerx1/discussionforum/ui/CategoryDetailActivity.java
@@ -2,14 +2,18 @@ package com.pioneerx1.discussionforum.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.pioneerx1.discussionforum.R;
 
 public class CategoryDetailActivity extends AppCompatActivity {
 
+    public static final String TAG = CategoryDetailActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_detail);
+        Log.v(TAG, "-------DETAIL ACTIVITY REACHED!!!");
     }
 }
