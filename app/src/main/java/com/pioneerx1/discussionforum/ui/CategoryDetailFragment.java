92df3c7913d8f682e48d4d1d66bb609fc03a4c8a
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-19 16:04:40 -0700

    update build.gradle (Module app) so android:support:support has 25.3.1 like appcompat and recyclerview, error build otherwise

---
+++ app/src/main/java/com/pioneerx1/discussionforum/ui/CategoryDetailFragment.java
@@ -0,0 +1,30 @@
+package com.pioneerx1.discussionforum.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pioneerx1.discussionforum.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CategoryDetailFragment extends Fragment {
+
+
+    public CategoryDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category_detail, container, false);
+    }
+
+}
