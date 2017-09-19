9c25b220685a437c09a0031172bf97188ec9ed50
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-19 10:03:33 -0700

    add migration from categorylist activity to add new category activity

--- app/src/main/java/com/pioneerx1/discussionforum/ui/CategoryListActivity.java
+++ app/src/main/java/com/pioneerx1/discussionforum/ui/CategoryListActivity.java
@@ -1,15 +1,34 @@
 package com.pioneerx1.discussionforum.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.pioneerx1.discussionforum.R;
 
-public class CategoryListActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CategoryListActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.newCategoryButton) Button mNewCategoryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_list);
+        ButterKnife.bind(this);
+
+        mNewCategoryButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNewCategoryButton) {
+            Intent intent = new Intent(CategoryListActivity.this, NewCategoryActivity.class);
+            startActivity(intent);
+        }
     }
 }
