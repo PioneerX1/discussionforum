03dea4f351d59aa8aec8c8af7402302f30542204
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-19 09:53:58 -0700

    add migration from Main to CategoryList activity

--- app/src/main/java/com/pioneerx1/discussionforum/ui/MainActivity.java
+++ app/src/main/java/com/pioneerx1/discussionforum/ui/MainActivity.java
@@ -1,15 +1,36 @@
 package com.pioneerx1.discussionforum.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.pioneerx1.discussionforum.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.enterButton) Button mEnterButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mEnterButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mEnterButton) {
+            Intent intent = new Intent(MainActivity.this, CategoryListActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
