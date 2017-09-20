4a0c6bef457a58c28eb86f0eb80e01c09a1953dd
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-20 10:46:45 -0700

    add message posts, currently posting for all categories, and also need to style more

---
+++ app/src/main/java/com/pioneerx1/discussionforum/adapters/FirebaseMessageViewHolder.java
@@ -0,0 +1,78 @@
+package com.pioneerx1.discussionforum.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.pioneerx1.discussionforum.Constants;
+import com.pioneerx1.discussionforum.R;
+import com.pioneerx1.discussionforum.models.Message;
+import com.pioneerx1.discussionforum.ui.CategoryDetailActivity;
+import com.pioneerx1.discussionforum.ui.MessageDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+public class FirebaseMessageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    private static final int MAX_WIDTH = 200;
+    private static final int MAX_HEIGHT = 200;
+
+    View mView;
+    Context mContext;
+
+    public FirebaseMessageViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindMessage(Message message) {
+        TextView messageTitleAndAuthorTextView = (TextView) mView.findViewById(R.id.messageTitleAndAuthorTextView);
+        TextView messageContentTextView = (TextView) mView.findViewById(R.id.messageContentTextView);
+
+        messageTitleAndAuthorTextView.setText(message.getTitle() + "  by: " + message.getAuthor());
+        messageContentTextView.setText(message.getContent());
+    }
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList<Message> messages = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_MESSAGE);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    messages.add(snapshot.getValue(Message.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                // NEED CODE FOR NEXT INTENT HERE
+                Intent intent = new Intent(mContext, MessageDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("messages", Parcels.wrap(messages));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+
+        });
+
+    }
+
+}
--- app/src/main/java/com/pioneerx1/discussionforum/ui/CategoryDetailActivity.java
+++ app/src/main/java/com/pioneerx1/discussionforum/ui/CategoryDetailActivity.java
@@ -3,11 +3,24 @@ package com.pioneerx1.discussionforum.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.pioneerx1.discussionforum.Constants;
 import com.pioneerx1.discussionforum.R;
+import com.pioneerx1.discussionforum.adapters.CategoryListAdapter;
+import com.pioneerx1.discussionforum.adapters.FirebaseCategoryViewHolder;
+import com.pioneerx1.discussionforum.adapters.FirebaseMessageViewHolder;
+import com.pioneerx1.discussionforum.models.Category;
+import com.pioneerx1.discussionforum.models.Message;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -15,7 +28,12 @@ import butterknife.ButterKnife;
 public class CategoryDetailActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = CategoryDetailActivity.class.getSimpleName();
+    public static final String categoryName = "Banjos";  // hard-coded for now
 
+    private DatabaseReference mMessageReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
+    @Bind(R.id.messageRecyclerView) RecyclerView mRecyclerView;
     @Bind(R.id.newMessageButton) Button mNewMessageButton;
 
     @Override
@@ -26,6 +44,8 @@ public class CategoryDetailActivity extends AppCompatActivity implements View.On
         Log.v(TAG, "-------DETAIL ACTIVITY REACHED!!!");
 
         mNewMessageButton.setOnClickListener(this);
+        mMessageReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_MESSAGE);
+        setUpFirebaseAdapter();
     }
 
     @Override
@@ -34,5 +54,25 @@ public class CategoryDetailActivity extends AppCompatActivity implements View.On
             Intent intent = new Intent(CategoryDetailActivity.this, NewMessageActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Message, FirebaseMessageViewHolder>
+                (Message.class, R.layout.message_list_item, FirebaseMessageViewHolder.class, mMessageReference) {
+
+            @Override
+            protected void populateViewHolder(FirebaseMessageViewHolder viewHolder, Message model, int position) {
+                viewHolder.bindMessage(model);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
     }
 }
